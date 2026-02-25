1b87056cff6a7e77646a36b1fa5a6cbb5b2156e6
wellz3280 <welingtonzsilva@gmail.com>
2026-02-24 22:45:28 -0300

    add borda ao conteudo de footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,10 @@ import styles from './styles.module.css';
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <p>2026 - José Weliton</p>
+            <p>
+                <span>&copy;</span> 
+                2026 José Weliton
+            </p>
 
         </div>
     );
